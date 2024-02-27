83be3ca61ece5cf17775b72ced4fbd9bfc56a904
vchuryukaev <v.churiukaev@etpgpb.ru>
2024-02-27 08:52:39 +0500

    feat(PIVO-1): Init project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from "@angular/router";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { NgModule } from "@angular/core";
+import { MainPageComponent } from "./main-page/main-page.component";
+
+const routes: Routes = [
+    { path: '', component: MainPageComponent },
+    { path: 'login', component: LoginPageComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+
+export class AppRoutingModule { }
